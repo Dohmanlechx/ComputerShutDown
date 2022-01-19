dbf34479d1829d5b420a902b158763c95c3e9096
David Öhman <dohman_92@hotmail.com>
2022-01-20 00:07:46 +0100

    Initial commit

---
+++ ComputerShutDown/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ComputerShutDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process pr = new Process();
+            pr.StartInfo.FileName = "shutdown";
+            pr.StartInfo.UseShellExecute = false;
+            pr.StartInfo.Arguments = "-s -t 10";
+            pr.StartInfo.RedirectStandardOutput = true;
+            pr.Start();
+        }
+    }
+}
